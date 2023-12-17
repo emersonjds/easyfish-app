c75e93cf567fb540e416d55fc574afb0cb59ba89
emersonjds <emerson_jds@live.com>
2023-12-17 11:35:41 -0300

    adjust faz area

--- src/pages/Faq/components/FazAccordion/index.tsx
+++ src/pages/Faq/components/FazAccordion/index.tsx
@@ -3,33 +3,33 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Collapsible from 'react-native-collapsible';
 
 interface FAQAccordionProps {
-    id: number;
-    question: string;
-    answer: string;
-    activeQuestion: number;
-    setActiveQuestion: (id: number) => void;
+  id: number;
+  question: string;
+  answer: string;
+  activeQuestion: number;
+  setActiveQuestion: (id: number) => void;
 
 }
 
 const FAQAccordion = ({ id, question, answer, activeQuestion, setActiveQuestion }) => {
-    const isCollapsed = id !== activeQuestion;
+  const isCollapsed = id !== activeQuestion;
 
-    const toggleAccordion = () => {
-        setActiveQuestion(id);
-    };
+  const toggleAccordion = () => {
+    setActiveQuestion(id);
+  };
 
-    return (
-        <View className="border-b border-gray-200">
-            <TouchableOpacity onPress={toggleAccordion} className="p-4">
-                <Text className="text-lg font-bold">{question}</Text>
-            </TouchableOpacity>
-            <Collapsible collapsed={isCollapsed}>
-                <View className="p-4">
-                    <Text className="text-sm">{answer}</Text>
-                </View>
-            </Collapsible>
+  return (
+    <View className="border-b border-gray-200">
+      <TouchableOpacity onPress={toggleAccordion} className="p-4">
+        <Text className="text-lg font-bold">{question}</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={isCollapsed}>
+        <View className="p-4">
+          <Text className="text-sm">{answer}</Text>
         </View>
-    );
+      </Collapsible>
+    </View>
+  );
 };
 
 export default FAQAccordion;
