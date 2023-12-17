dd48000ca494fe5ec174498f2e22e2c868e17947
emersonjds <emerson_jds@live.com>
2023-12-17 11:34:45 -0300

    add faz to app

---
+++ src/pages/Faq/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import { cultivoTilapia } from './data';
+import FAQAccordion from './components/FazAccordion';
+
+const FAQScreen = () => {
+	const [activeQuestion, setActiveQuestion] = useState(null);
+
+	return (
+
+		<>
+			<View
+				className="flex-1 px-5 mt-10"
+			>
+				<Text className="text-2xl font-bold ">Perguntas Frequentes</Text>
+				<ScrollView className="p-4">
+					{cultivoTilapia.map((faq) => (
+						<FAQAccordion
+							key={faq.id}
+							id={faq.id}
+							question={faq.pergunta}
+							answer={faq.resposta}
+							activeQuestion={activeQuestion}
+							setActiveQuestion={setActiveQuestion}
+						/>
+					))}
+				</ScrollView>
+			</View>
+		</>
+	);
+};
+
+export default FAQScreen;
